225aeda129b256d08eb5f634ba88256d7cf57962
Rizart Dona <rizart.dona@gmail.com>
2020-09-21 14:20:08 +0200

    add with clause for automatic file closure

--- cric-info-tools/export_endpoints_bash.py
+++ cric-info-tools/export_endpoints_bash.py
@@ -26,29 +26,26 @@ def main():
                         datefmt='%d/%m/%Y %I:%M:%S %p', level=logging.INFO)
     logger = logging.getLogger()
 
-    export_file = open(export_file, "w+")
-    export_file.write("#!/bin/bash\n\n")
+    with open(export_file, 'w+') as fp:
+        fp.write("#!/bin/bash\n\n")
+        # export bash info
+        cric_rse_data = requests.get(CRIC_RSES_URL).json()
+        for rse in cric_rse_data:
+            logger.info("Exporting {}".format(rse))
+            rse_label = rse.replace("-", "_").lower()
+            fp.write("# {}\n".format(rse))
+            protocols = cric_rse_data[rse]['protocols']
+            for protocol_label in protocols:
+                protocol = protocols[protocol_label]
+                protocol_name = protocol['flavour']
+                endpoint = protocol['endpoint']
+                path = protocol['path']
+                endpoint_url = "{}://{}{}".format(protocol_name,
+                                                  endpoint, path)
+                fp.write("export {}_{}_{}='{}'\n".format(
+                    EXPORT_PREFIX, rse_label, protocol_name, endpoint_url))
 
-    # export bash info
-    cric_rse_data = requests.get(CRIC_RSES_URL).json()
-    for rse in cric_rse_data:
-        logger.info("Exporting {}".format(rse))
-        rse_label = rse.replace("-", "_").lower()
-        export_file.write("# {}\n".format(rse))
-        protocols = cric_rse_data[rse]['protocols']
-        for protocol_label in protocols:
-            protocol = protocols[protocol_label]
-            protocol_name = protocol['flavour']
-            endpoint = protocol['endpoint']
-            path = protocol['path']
-            endpoint_url = "{}://{}{}".format(protocol_name, endpoint, path)
-            export_file.write("export {}_{}_{}='{}'\n".format(
-                EXPORT_PREFIX, rse_label, protocol_name, endpoint_url))
-
-        export_file.write("\n")
-        logger.info("{} exporting DONE".format(rse))
-
-    export_file.close()
+            fp.write("\n")
 
 
 if __name__ == '__main__':
