dd6adbb443d90aada47de281b9781a33e2f4a27d
Riccardo Di Maria <riccardo.di.maria@cern.ch>
2021-04-30 18:16:35 +0200

    Preparing for escape-gitops

--- update-proxies/propagate_proxies.sh
+++ update-proxies/propagate_proxies.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 
 HOMEPATH="/afs/cern.ch/user/r/rdimaria"
-KUBERNETESCLUSTER="${HOMEPATH}/escape-wp2-k8s"
+KUBERNETESCLUSTER="${HOMEPATH}/escape-gitops/"
+
+RELEASENAMEDAEMONS="escape-daemons"
 
 # Propagates proxy to FTS and Kubernetes Cluster
 export X509_USER_PROXY=${HOMEPATH}/proxies/x509up_escape
-eval $(ai-rc "ESCAPE WP2 CERN")
-export KUBECONFIG=${KUBERNETESCLUSTER}/k8s_cluster_config
+
+export KUBECONFIG=${KUBERNETESCLUSTER}/escape-gitops_config
 cd ${KUBERNETESCLUSTER}
 cp ${X509_USER_PROXY} x509up
-for OUTPUT in $(kubectl get pods | cut -d " " -f1 | grep daemons)
+for OUTPUT in $(kubectl -n rucio get pods | cut -d " " -f1 | grep daemons)
 do
     echo $OUTPUT
-    kubectl cp x509up $OUTPUT:/x509up
+    kubectl -n rucio cp x509up $OUTPUT:/x509up
 done
-kubectl delete secret prod-rucio-x509up
-kubectl create secret generic prod-rucio-x509up --from-file=x509up
+kubectl delete secret ${RELEASENAMEDAEMONS}-rucio-x509up -n rucio
+kubectl create secret generic ${RELEASENAMEDAEMONS}-rucio-x509up --from-file=x509up -n rucio
 #scp /afs/cern.ch/user/a/afkiaras/proxies/x509up_escape root@escape-jupyter-lab-rucio:/root/x509up
 ssh lxplus fts-delegation-init -s https://fts3-pilot.cern.ch:8446 --proxy ${X509_USER_PROXY}
